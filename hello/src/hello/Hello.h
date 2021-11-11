b0f94873412e55cbd1da7b7cf92f5d46fc531945
Seain Malkin <dev@seain.me>
2021-11-11 12:12:12 +0800

    Initial commit

---
+++ hello/src/hello/Hello.h
@@ -0,0 +1,11 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+class Hello
+{
+public:
+	Hello();
+	static void say();
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Main\n";
+
+	return 0;
+}
