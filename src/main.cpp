3292a7878baeb67cef6dbc7f822a125f27089d97
Seain Malkin <dev@seain.me>
2021-11-15 07:18:51 +0800

    Changed library linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 
-#include <etc/stb_image.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <stb/stb_image.h>
 
 int main()
 {
 	
+	glfwInit();
+	/**
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 400, "BreakOut", NULL, NULL);
+
+	if (window == NULL)	
+	{
+		std::cout << "Failed to create window." << std::endl;
+		return 1;
+	}
+
+	glfwTerminate();
+	*/
 
 	return 0;
 }
--- src/stb_imp.cpp
+++
@@ -1,2 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include <etc/stb_image.h>
