e41a19df7c54d5adb188937a291a8f6258830bae
Seain Malkin <dev@seain.me>
2021-11-15 19:10:00 +0800

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,12 @@
 
 int main()
 {
-	
 	glfwInit();
-	
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow *window = glfwCreateWindow(800, 400, "BreakOut", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(800, 400, "Break Out", NULL, NULL);
 
 	if (window == NULL)	
 	{
