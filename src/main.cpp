ca1ad1ced34eaae3e87b301fdc4f1077cbb5c302
Seain Malkin <dev@seain.me>
2021-11-18 09:47:34 +0800

    Removed unused includes

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <stb/stb_image.h>
 
 #include <oglbuffer/ObjectBuffer.h>
 #include <oglasset/Shader.h>
