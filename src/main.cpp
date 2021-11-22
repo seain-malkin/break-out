cbebdeb6277c998a3b533a741596227f6e0b802b
Seain Malkin <dev@seain.me>
2021-11-23 04:12:47 +0800

    Build automation

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <oglengine/Engine.h>
 #include <oglbuffer/ObjectBuffer.h>
